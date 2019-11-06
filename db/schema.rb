aa211514bac84b38d2ef633bd0f86843e892f4cc
Derek Cerretani <drkjc@protonmail.com>
2019-11-06 10:56:38 -0700

    create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_191321) do
+ActiveRecord::Schema.define(version: 2019_11_06_175549) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2019_10_30_191321) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.boolean "rented"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
