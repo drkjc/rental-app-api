bd8d5f9f12efeed4bb04b9c79637758e2326ed1a
Derek Cerretani <drkjc@protonmail.com>
2019-11-09 11:58:44 -0700

    added shelves/:id/items route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :items, :shelves    
+    resources :items, :shelves do 
+      resources :items 
+    end 
   end
 end
