71c5a681f707a04973d63a1761a870a29f2a8988
drkjc <drkjc@protonmail.com>
2020-03-21 14:14:25 -0700

    fixed start and end date params

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,38 +1,39 @@
-class Api::ItemsController < ApplicationController 
+class Api::ItemsController < ApplicationController
   def index
     if params[:shelf_id]
       @shelf = Shelf.find(params[:shelf_id])
-      render json: @shelf.items 
-    elsif params[:cart_id] 
+      render json: @shelf.items
+    elsif params[:cart_id]
       @cart = Cart.find(params[:cart_id])
       #binding.pry
-      render json: @cart.items 
+      render json: @cart.items
     end
   end
 
-  def show 
+  def show
     @item = Item.find(params[:id])
     render json: @item
   end
 
   def create
     # renter_id = User.find(1)
+    binding.pry
     if params[:cart_id]
-      @item = Item.find(params[:item][:item][:id]) 
+      @item = Item.find(params[:item][:item][:id])
 
       @item.cart_id = params[:cart_id]
-      @item.start_date = params[:item][:startDate]
-      @item.end_date = params[:item][:endDate]
-      @item.rented = true 
-      @item.save 
+      @item.start_date = params[:item][:start]
+      @item.end_date = params[:item][:end]
+      @item.rented = true
+      @item.save
       render json: @item
     elsif params[:shelf_id]
       @shelf = Shelf.find(params[:shelf_id])
       @item = Item.new(item_params)
       @item.cart_id = params[:shelf_id]
       @item.shelf_id = params[:shelf_id]
-      @item.rented = false 
-      @item.save 
+      @item.rented = false
+      @item.save
       @shelf.items << @item
       render json: @shelf.items
     end
@@ -43,11 +44,11 @@ class Api::ItemsController < ApplicationController
     @item.name = params[:item][:name]
     @item.price = params[:item][:price]
     @item.category = params[:item][:category]
-    @item.save 
+    @item.save
     render json: @item
   end
 
-  def destroy 
+  def destroy
     if params['from']['shelf']
       @shelf = Shelf.find(params[:shelf_id])
       @item = Item.find(params[:id])
@@ -63,13 +64,13 @@ class Api::ItemsController < ApplicationController
       @item.cart_id = @item.shelf_id
       @item.start_date = ''
       @item.end_date = ''
-      @item.rented  = false 
-      @item.save 
+      @item.rented  = false
+      @item.save
       render json: @cart.items
     end
   end
 
-  private 
+  private
 
   def item_params
     params.require(:item).permit(:name, :category, :price)
